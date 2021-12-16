761ba4cf7669f7c22e19a2a469449ff670ad28b0
Durdiev9043 <donyorber9043@gmail.com>
2021-12-16 16:49:14 +0500

    price crud add

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Price;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PriceController extends Controller
 {
@@ -32,11 +33,23 @@ class PriceController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'summ'=>'required',
+        ]);
+        if ($validator->fails()){
+            return response()->json($validator->error())->with('error','Validation Error.');
+        }
+
+        $price=Price::create($request->all());
+        if ($price){
+            return redirect()->back();
+        }
+
     }
 
     /**
@@ -66,21 +79,33 @@ class PriceController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Price  $price
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
      */
     public function update(Request $request, Price $price)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'summ'=>'required',
+        ]);
+        if ($validator->fails()){
+            return response()->json($validator->error())->with('error','Validation Error.');
+        }
+
+        $prices=$price->fill($request->validated())->update();
+        if ($prices){
+            return redirect()->back();
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Price  $price
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
      */
     public function destroy(Price $price)
     {
-        //
+        $delete=$price->delete();
+        return redirect()->back();
     }
 }
