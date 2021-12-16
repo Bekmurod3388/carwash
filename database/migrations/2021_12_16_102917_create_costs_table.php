efef6caa719f674721008309adde1e287c8a825d
Durdiev9043 <donyorber9043@gmail.com>
2021-12-16 15:39:21 +0500

    cost migrate and model

---
+++ database/migrations/2021_12_16_102917_create_costs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('costs', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->bigInteger("sum");
+            $table->bigInteger("count");
+            $table->bigInteger("total-sum");
+            $table->string("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('costs');
+    }
+}
